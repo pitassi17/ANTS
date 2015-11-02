7b004a8c4d83c698c54d4201b41983765e74b20f
Bryce Matsuda <iexist4jcore@gmail.com>
2015-11-01 20:29:28 -0800

    Add main ANTS class.

---
+++ Program/ANTS/src/main/Ants.java
@@ -0,0 +1,19 @@
+package main;
+
+/**
+ * Main/mother class for ANTS. 
+ * 
+ * Arguments from command line are gathered from this class.
+ * 
+ * @author Bryce Matsuda
+ *
+ */
+public class Ants {
+
+	public static void main(String[] args) {
+		for (String arg : args){
+			System.out.println(arg);
+		}
+	}
+
+}
